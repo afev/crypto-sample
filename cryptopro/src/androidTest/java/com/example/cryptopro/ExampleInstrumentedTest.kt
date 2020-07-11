c178b14ef9aaae297ad5b5e529bf101c5a426e78
pavel.novikov <pavel.novikov@myoffice.ru>
2020-07-11 16:46:02 +0300

    Add cryptpro module

---
+++ cryptopro/src/androidTest/java/com/example/cryptopro/ExampleInstrumentedTest.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) New Cloud Technologies, Ltd., 2013-2020
+ *
+ * You can not use the contents of the file in any way without New Cloud Technologies, Ltd. written permission.
+ * To obtain such a permit, you should contact New Cloud Technologies, Ltd. at http://ncloudtech.com/contact.html
+ *
+ */
+
+package com.example.cryptopro
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.cryptopro.test", appContext.packageName)
+    }
+}
---
+++ cryptopro/src/test/java/com/example/cryptopro/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) New Cloud Technologies, Ltd., 2013-2020
+ *
+ * You can not use the contents of the file in any way without New Cloud Technologies, Ltd. written permission.
+ * To obtain such a permit, you should contact New Cloud Technologies, Ltd. at http://ncloudtech.com/contact.html
+ *
+ */
+
+package com.example.cryptopro
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
